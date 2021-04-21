b0735b4e551d92169dc57e7be347b74f9ab24565
CXO <amincxo@gmail.com>
2021-04-21 18:17:52 +0430

    debug hide and side bar

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function hideme() {
     if (document.getElementById("bt1").value=="Showbar") {
         document.getElementById("bt1").value="Hidebar"
-        document.getElementById("menubar_mobile").style.display="unset"
+        document.getElementById("menubar_mobile").style.display="g"
         document.getElementById("menubar_mobile").style.backgroundColor="#FC766AFF"
-
+        document.getElementById("menubar_mobile").style.position="absolute"
+        document.getElementById("menubar_mobile").style.top="50px"
+        document.getElementById("menubar_mobile").style.left="0px"
 
             }
             else{
                 document.getElementById("bt1").value="Showbar"
-                document.getElementById("menubar_mobile").style.display="hide"
+                document.getElementById("menubar_mobile").style.position="absolute"
+                document.getElementById("menubar_mobile").style.top="0px"
+                document.getElementById("menubar_mobile").style.left="-310px"
+
             }
 }
